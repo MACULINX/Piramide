72cba9c83f52a823738bd21f69ccb0a53f7754b1
Marco Bucci <56220142+MACULINX@users.noreply.github.com>
2023-05-05 13:34:58 +0200

    Update piramide.cs

--- Bucci.Marco.3i.piramide/piramide.cs
+++ Bucci.Marco.3i.piramide/piramide.cs
@@ -10,7 +10,7 @@ namespace Piramide{
                 return 0;
             
             bool flag = true;
-            int piani = 1;
+            int piani = 0;
             int mattnec = 0;
             int matt = 1;
 
@@ -27,7 +27,7 @@ namespace Piramide{
                 
             }
             
-            return piani-1;
+            return piani;
         }
         public static int Rimanenti( int mattoni )
         {
@@ -35,7 +35,6 @@ namespace Piramide{
                 return 0;
             
             bool flag = true;
-            int piani = 1;
             int mattnec = 0;
             int matt = 1;
 
@@ -44,10 +43,8 @@ namespace Piramide{
                 flag = (Math.Pow((matt),2)) + mattnec <= mattoni ? true : false;
                 
                 if(flag){
-
                     mattnec = mattnec + (int)(Math.Pow((matt),2));
                     matt+=2;
-                    piani++;
                 }
                 
             }
